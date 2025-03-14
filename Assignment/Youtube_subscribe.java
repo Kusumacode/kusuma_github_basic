0193156c92c7022800b7dfbe746973cf7cad3239
Kusuma <userkusuma2065@gmail.com>
2025-03-14 21:51:55 +0530

    Date and Time Concept

--- Assignment/Youtube_subscribe.java
+++ Assignment/Youtube_subscribe.java
@@ -11,14 +11,13 @@ public class Youtube_subscribe
 {
 	public static void main(String[] args) throws InterruptedException
 	{
-		EdgeDriver driver=new EdgeDriver();
-		Thread.sleep(3000);
-		/* Using i-frame: Using TestNG 
+		ChromeDriver driver=new ChromeDriver();
+
+		/* Using i frame: Using TestNG 
 		1. Launch Google
 		2. Type"Grotechminds" and press enter button */
 		driver.get("https://www.google.co.in/");
 		driver.manage().window().maximize();
-		Thread.sleep(3000);
 		WebElement GTM=driver.findElement(By.id("APjFqb"));
 		GTM.sendKeys("GroTechminds");
 		GTM.sendKeys(Keys.ENTER);
@@ -26,7 +25,7 @@ public class Youtube_subscribe
 		//3. Click on hamburger button available on right TOP corner(9 dots)
 		WebElement hamburger=driver.findElement(By.xpath("//div[@class='gb_C']"));
 		hamburger.click();
-	
+
 		WebElement icon=driver.findElement(By.name("app"));
 		driver.switchTo().frame(icon);
 		
@@ -34,13 +33,13 @@ public class Youtube_subscribe
 		WebElement youtube=driver.findElement(By.xpath("//div[@class='LVal7b ']/ul/li[4]"));
 		youtube.click();
 		
-		//5. Search for GroTechminds
+		//5. Search for grotechminds
 		WebElement search1=driver.findElement(By.name("search-query"));
 		search1.sendKeys("GroTechMinds");
 		search1.sendKeys(Keys.ENTER);
-	
+
 		//6. Click on "Subscribe" button of GroTechMinds you tube channel 
 		WebElement subscribe=driver.findElement(By.xpath("//div[@class='yt-spec-touch-feedback-shape__fill'][6]"));
 		subscribe.click();
-}
+	}
 }
